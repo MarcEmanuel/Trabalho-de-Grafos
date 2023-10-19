29ad8ec9cdf9db2398ae58a822fc56f299787033
Marcos Emanuel <maemspp@gmail.com>
2023-10-19 00:31:45 -0300

    Resolvendo Segmetation fault

--- prim/prim.c
+++ prim/prim.c
@@ -2,298 +2,149 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <limits.h>
 
-// Estrutura para representar um vértice
 struct No {
     int vertice;
     int peso;
     struct No* prox;
-} typedef No;
-
-// Estrutura para representar uma aresta
-struct ouNO {
-    int vim, vou, peso;
-} typedef ouNO;
-
-// Estrutura para representar um nó no heap (fila de prioridade)
-struct HeapNode {
-    int vertice;
-    int peso;
 };
 
-// Função auxiliar para criar um heap vazio
-struct HeapNode* createHeap(int capacity) {
-    return (struct HeapNode*)malloc(capacity * sizeof(struct HeapNode));
+struct ouNO {
+    int vim, vou, peso;
+};
+
+// Função auxiliar para adicionar uma aresta a uma lista de adjacência
+void addNo(struct No** lista, int vertice, int peso) {
+    struct No* novo = (struct No*)malloc(sizeof(struct No));
+    novo->vertice = vertice;
+    novo->peso = peso;
+    novo->prox = *lista;
+    *lista = novo;
 }
 
-// Função auxiliar para trocar dois elementos em um heap
-void swapHeapNodes(struct HeapNode* x, struct HeapNode* y) {
-    struct HeapNode temp = *x;
-    *x = *y;
-    *y = temp;
+// Função auxiliar para liberar a memória de uma lista de adjacência
+void liberarLista(struct No* lista) {
+    while (lista != NULL) {
+        struct No* temp = lista;
+        lista = lista->prox;
+        free(temp);
+    }
 }
 
-// Função para corrigir o heap após adicionar um novo elemento
-void heapifyUp(struct HeapNode* heap, int index) {
-    while (index > 0) {
-        int parentIndex = (index - 1) / 2;
-        if (heap[index].peso < heap[parentIndex].peso) {
-            swapHeapNodes(&heap[index], &heap[parentIndex]);
-            index = parentIndex;
-        } else {
+// Função auxiliar para mostrar o caminho da árvore geradora mínima
+void mostrarCaminho(struct ouNO* arestas, int numArestas) {
+    printf("Caminho da árvore geradora mínima:\n");
+    for (int i = 0; i < numArestas; i++) {
+        printf("Aresta (%d,%d) com peso %d\n", arestas[i].vim, arestas[i].vou, arestas[i].peso);
+    }
+}
+
+// Função para implementar o algoritmo de Prim
+void prim(struct No** lista, int numVertices, struct ouNO* arestas) {
+    int* pai = (int*)malloc(numVertices * sizeof(int));
+    int* pesoMinimo = (int*)malloc(numVertices * sizeof(int));
+    bool* naArvore = (bool*)malloc(numVertices * sizeof(bool));
+
+    for (int i = 0; i < numVertices; i++) {
+        pai[i] = -1;
+        pesoMinimo[i] = INT_MAX;
+        naArvore[i] = false;
+    }
+
+    int inicioVertice = 0;  // Começando a partir do vértice 0 (você pode escolher o vértice inicial desejado)
+    pesoMinimo[inicioVertice] = 0;
+
+    for (int count = 0; count < numVertices - 1; count++) {
+        int verticeAtual = -1;
+        for (int v = 0; v < numVertices; v++) {
+            if (!naArvore[v] && (verticeAtual == -1 || pesoMinimo[v] < pesoMinimo[verticeAtual])) {
+                verticeAtual = v;
+            }
+        }
+
+        if (verticeAtual == -1) {
+            // Não foi possível encontrar um vértice, algo deu errado
             break;
         }
-    }
-}
 
-// Função para adicionar um elemento ao heap
-void insert(struct HeapNode* heap, int* heapSize, int vertice, int peso) {
-    heap[*heapSize].vertice = vertice;
-    heap[*heapSize].peso = peso;
-    heapifyUp(heap, (*heapSize)++);
-}
+        naArvore[verticeAtual] = true;
 
-// Função auxiliar para corrigir o heap após remover um elemento
-void heapifyDown(struct HeapNode* heap, int heapSize, int index) {
-    int smallest = index;
-    int left = 2 * index + 1;
-    int right = 2 * index + 2;
-
-    if (left < heapSize && heap[left].peso < heap[smallest].peso) {
-        smallest = left;
-    }
-
-    if (right < heapSize && heap[right].peso < heap[smallest].peso) {
-        smallest = right;
-    }
-
-    if (smallest != index) {
-        swapHeapNodes(&heap[index], &heap[smallest]);
-        heapifyDown(heap, heapSize, smallest);
-    }
-}
-
-// Função para extrair o elemento mínimo (o topo) do heap
-struct HeapNode extractMin(struct HeapNode* heap, int* heapSize) {
-    struct HeapNode min = heap[0];
-    heap[0] = heap[--(*heapSize)];
-    heapifyDown(heap, *heapSize, 0);
-    return min;
-}
-
-// Função para verificar se o heap está vazio
-bool isHeapEmpty(int heapSize) {
-    return heapSize == 0;
-}
-
-void addNo(No* no[], int x1, int x2, int peso) {
-    No* new = (No*)malloc(sizeof(No));
-    new->vertice = x2;
-    new->peso = peso;
-    new->prox = NULL;
-
-    if (no[x1] == NULL) {
-        no[x1] = new;
-        return;
-    } else {
-        No* Aux = no[x1];
-        while (Aux->prox != NULL) {
-            Aux = Aux->prox;
-        }
-        Aux->prox = new;
-    }
-}
-
-int compararVertices(const void* i, const void* j) {
-    struct ouNO* ir = (struct ouNO*)i;
-    struct ouNO* volta = (struct ouNO*)j;
-    if (ir->vim == volta->vim) {
-        return (ir->vou - volta->vou);
-    }
-    return (ir->vim - volta->vim);
-}
-
-void MostrarCaminho(ouNO vertices[], bool ordem, int numVert, int cost, bool mostrarTela, FILE* out) {
-    int i;
-    if (ordem) {
-        qsort(vertices, numVert - 1, sizeof(ouNO), compararVertices);
-
-        if (mostrarTela) {
-            for (i = 0; i < numVert - 1; i++) {
-                printf("(%d,%d) ", vertices[i].vim, vertices[i].vou);
-            }
-        } else {
-            for (i = 0; i < numVert - 1; i++) {
-                fprintf(out, "(%d,%d) ", vertices[i].vim, vertices[i].vou);
-            }
-            fclose(out);
-        }
-    }
-    if (!ordem && mostrarTela == true) {
-        printf("%d\n", cost);
-    }
-    if (!ordem && !mostrarTela) {
-        fprintf(out, "%d\n", cost);
-        fclose(out);
-    }
-}
-
-bool Visitas(int vetor[], int num) {
-    int i;
-    for (i = 0; i <= num; i++) {
-        if (vetor[i] == false)
-            return false;
-    }
-    return true;
-}
-
-void Prim(No* no[], ouNO ouno[], int inicioVertice, int tamVertice, int* aux) {
-    int count = 0;
-    int i, prox, pesin, va, cost = 0;
-
-    int* peso = (int*)malloc(tamVertice * sizeof(int));
-    int* visitado = (int*)malloc(tamVertice * sizeof(int));
-    int* proximo = (int*)malloc(tamVertice * sizeof(int));
-
-    for (i = 0; i <= tamVertice; i++) {
-        peso[i] = 99999;
-        visitado[i] = false;
-        proximo[i] = -1;
-    }
-
-    peso[inicioVertice] = 0;
-
-    // Crie um heap mínimo para selecionar a próxima aresta
-    int heapSize = 0;
-    struct HeapNode* heap = createHeap(tamVertice);
-
-    // Insira o vértice de início no heap
-    insert(heap, &heapSize, inicioVertice, peso[inicioVertice]);
-
-    while (!isHeapEmpty(heapSize)) {
-        // Extraia o vértice de menor peso do heap
-        struct HeapNode minNode = extractMin(heap, &heapSize);
-        va = minNode.vertice;
-
-        if (proximo[va] != -1) {
-            cost += peso[va];
-
-            ouno[count].vim = proximo[va];
-            ouno[count].vou = va;
-            ouno[count].peso = peso[va];
-            count++;
-        }
-
-        No* aux = no[va];
+        struct No* aux = lista[verticeAtual];
         while (aux != NULL) {
-            prox = aux->vertice;
-            pesin = aux->peso;
+            int v = aux->vertice;
+            int peso = aux->peso;
 
-            if (!visitado[prox] && peso[prox] > pesin) {
-                peso[prox] = pesin;
-                proximo[prox] = va;
-
-                // Insira o vértice no heap
-                insert(heap, &heapSize, prox, pesin);
+            if (!naArvore[v] && peso < pesoMinimo[v]) {
+                pai[v] = verticeAtual;
+                pesoMinimo[v] = peso;
             }
             aux = aux->prox;
         }
     }
 
-    *aux = cost;
-    free(peso);
-    free(visitado);
-    free(proximo);
-    free(heap);
+    int numArestas = 0;
+    for (int i = 0; i < numVertices; i++) {
+        if (pai[i] != -1) {
+            arestas[numArestas].vim = pai[i];
+            arestas[numArestas].vou = i;
+            arestas[numArestas].peso = pesoMinimo[i];
+            numArestas++;
+        }
+    }
+
+    mostrarCaminho(arestas, numArestas);
+
+    free(pai);
+    free(pesoMinimo);
+    free(naArvore);
 }
 
 void help() {
-    printf("-o <arquivo> : redireciona a saida para o arquivo\n");
-    printf("-f <arquivo> : indica o arquivo que contem o grafo de entrada\n");
-    printf("-s : mostra a solucao (em ordem crescente)\n");
-    printf("-i : vertice inicial\n");
-}
-
-int Parametro(int argc, char* argv[], bool* mostrarCmd, bool* ordem, int* iniVertice, FILE** input, FILE** output) {
-    int i;
-    if (argc < 2) {
-        printf("Precione -h para saber informações\n");
-        return 0;
-    }
-
-    for (i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-h") == 0) {
-            help();
-            return 0;
-        }
-        if (strcmp(argv[i], "-o") == 0) {
-            *mostrarCmd = false;
-            if (i < argc - 1) {
-                char* nome = argv[i + 1];
-                *output = fopen(nome, "w");
-                if (*output == NULL) {
-                    printf("Erro ao abrir o arquivo.\n");
-                    return 0;
-                }
-                i++;
-            } else {
-                printf("Falta o arquivo\n");
-                return 0;
-            }
-        }
-        if (strcmp(argv[i], "-f") == 0) {
-            if (i < argc - 1) {
-                char* nome = argv[i + 1];
-                *input = fopen(nome, "r");
-                if (*input == NULL) {
-                    printf("Erro ao abrir o arquivo\n");
-                    return 0;
-                }
-                i++;
-            } else {
-                printf("Falta o arquivo de entrada\n");
-                return 0;
-            }
-        }
-        if (strcmp(argv[i], "-s") == 0) {
-            *ordem = true;
-        }
-        if (strcmp(argv[i], "-i") == 0) {
-            if (i < argc - 1) {
-                *iniVertice = atoi(argv[i + 1]);
-                i++;
-            } else {
-                printf("Falta valor inteiro\n");
-                return 0;
-            }
-        }
-    }
-    return 1;
+    printf("-f <arquivo> : indica o arquivo que contém o grafo de entrada\n");
 }
 
 int main(int argc, char* argv[]) {
-    int iniVertice = 1, numVertex, numArestas, x1, x2, peso, cost = 0;
-    bool ordem = false, mostrarCmd = true;
-    FILE* inputFile = NULL, *outputFile = NULL;
+    int numVertices, numArestas, x1, x2, peso;
+    FILE* inputFile = NULL;
 
-    if (Parametro(argc, argv, &mostrarCmd, &ordem, &iniVertice, &inputFile, &outputFile) != 1) {
-        return 0;
+    if (argc < 3) {
+        help();
+        return 1;
     }
 
-    fscanf(inputFile, "%d %d", &numVertex, &numArestas);
-    No* no[numVertex + 1];
-    ouNO ouno[numVertex - 1];
-    for (int i = 0; i <= numVertex; i++) {
-        no[i] = NULL;
+    if (strcmp(argv[1], "-f") != 0) {
+        help();
+        return 1;
     }
+
+    char* nomeArquivo = argv[2];
+    inputFile = fopen(nomeArquivo, "r");
+    if (inputFile == NULL) {
+        printf("Erro ao abrir o arquivo de entrada.\n");
+        return 1;
+    }
+
+    fscanf(inputFile, "%d %d", &numVertices, &numArestas);
+    struct No** listaAdjacencia = (struct No**)malloc(numVertices * sizeof(struct No*));
+    for (int i = 0; i < numVertices; i++) {
+        listaAdjacencia[i] = NULL;
+    }
+
     while (fscanf(inputFile, "%d %d %d", &x1, &x2, &peso) != EOF) {
-        addNo(no, x1, x2, peso);
-        addNo(no, x2, x1, peso);
+        addNo(&listaAdjacencia[x1], x2, peso);
+        addNo(&listaAdjacencia[x2], x1, peso); // Considere arestas bidirecionais
     }
 
-    Prim(no, ouno, iniVertice, numVertex, &cost);
-    MostrarCaminho(ouno, ordem, numVertex, cost, mostrarCmd, outputFile);
+    struct ouNO* arestas = (struct ouNO*)malloc((numVertices - 1) * sizeof(struct ouNO));
+    prim(listaAdjacencia, numVertices, arestas);
 
+    for (int i = 0; i < numVertices; i++) {
+        liberarLista(listaAdjacencia[i]);
+    }
+    free(listaAdjacencia);
+    free(arestas);
     fclose(inputFile);
+
     return 0;
 }
