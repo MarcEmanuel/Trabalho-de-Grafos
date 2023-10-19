ca5cd6a6c66f5b076d8689d9b6b524bca9071175
Marcos Emanuel <maemspp@gmail.com>
2023-10-18 23:10:58 -0300

    corrigindo Segmentation fault

--- bellmanFord/bellmanFord.c
+++ bellmanFord/bellmanFord.c
@@ -3,119 +3,104 @@
 #include <string.h>
 #include <unistd.h>
 
-#define INF 9999
+#define INF 999999999
 #define NULO -1
 #define TRUE 1
 #define FALSE 0
-#define MAX 100
 
-FILE *file;
+typedef struct {
+    int v1, v2, w;
+} Aresta;
 
-int grafo[MAX][MAX][2],d[MAX],p[MAX];
-int n,m;
+int n, m;
+Aresta *arestas;
+int *d, *p;
 
-void init(int s)
-{
-    int i;
-    for(i=0;i<n;i++)
-    {
-        d[i]=INF;
-        p[i]=NULO;
+void init(int s) {
+    for (int i = 0; i < n; i++) {
+        d[i] = INF;
+        p[i] = NULO;
     }
-    d[s]=0;
+    d[s] = 0;
 }
 
-void calculo(int u,int v)
-{
-    if(d[v]>(d[u]+grafo[u][v][1]))
-    {
-        d[v]=(d[u]+grafo[u][v][1]);
-        p[v]=u;
+void calculo(int u, int v, int w) {
+    if (d[v] > (d[u] + w)) {
+        d[v] = (d[u] + w);
+        p[v] = u;
     }
 }
 
-void caminho(int s, int v)
-{
-    if(v==s){
-        printf("  %d  ",s+1);
-    }
-    
-    else if(p[v]==NULO){
-        printf("\nNão existe caminho %d a %d \n",s+1,v+1);
-    }
-
-    else
-    {
-        caminho(s,p[v]);
-        printf("  %d  ",v+1);
+void caminho(int s, int v) {
+    if (v == s) {
+        printf("  %d  ", s + 1);
+    } else if (p[v] == NULO) {
+        printf("\nNão existe caminho %d a %d\n", s + 1, v + 1);
+    } else {
+        caminho(s, p[v]);
+        printf("  %d  ", v + 1);
     }
 }
 
-int bellmanFord(int s)
-{
-    int i,v1,v2;
+int bellmanFord(int s) {
     init(s);
-    for(i=0;i<(n-1);i++)
-    {
-        for(v1=0;v1<n;v1++){
-            for(v2=0;v2<n;v2++){
-                if(grafo[v1][v2][0]==1){
-                    calculo(v1,v2);
-                }
-            }
+    for (int i = 0; i < (n - 1); i++) {
+        for (int j = 0; j < m; j++) {
+            int v1 = arestas[j].v1;
+            int v2 = arestas[j].v2;
+            int w = arestas[j].w;
+            calculo(v1, v2, w);
         }
     }
-    
-    for(i=0;i<n;i++){
-        for(v1=0;v1<n;v1++){
-            if(grafo[i][v1][0]==1)
-            {
-                if(d[v1]>(d[i]+grafo[i][v1][1])){
-                    return FALSE;
-                }
-            }
+
+    for (int j = 0; j < m; j++) {
+        int v1 = arestas[j].v1;
+        int v2 = arestas[j].v2;
+        int w = arestas[j].w;
+        if (d[v2] > (d[v1] + w)) {
+            return FALSE;
         }
     }
     return TRUE;
 }
 
-int main(int argc, char *argv[])
-{
-    int h, f, i, l;
-    int v1,v2,c,s,v,w;
+int main(int argc, char *argv[]) {
+    int i, s, v;
 
-    if (argc == 1){
+    if (argc == 1) {
         printf("\nAdicione um texto de entrada! (ex: -f entrada.txt)\n");
-        exit(0);
+        return 1;
     }
 
     int opcao;
-    while((opcao = getopt(argc, argv, "help:f:")) != -1)
-	{
-		switch(opcao)
-		{
+    FILE *file = NULL;
+
+    while ((opcao = getopt(argc, argv, "hf:")) != -1) {
+        switch (opcao) {
             case 'h':
                 printf("\nUse o parâmetro -f para colocar a entrada. (ex: -f entrada.txt)\n");
                 return 0;
             case 'f':
                 file = fopen(optarg, "r");
-                if(file == NULL){
-                    printf("TXT vazio.\n");
-                    return 0;
+                if (file == NULL) {
+                    printf("Arquivo vazio ou inexistente.\n");
+                    return 1;
                 }
-                fscanf(file, "%d %d" , &n, &m);
+                fscanf(file, "%d %d", &n, &m);
 
-                for(i=0;i<m;i++)
-                {
-                    fscanf(file, "%d %d %d" , &v1, &v2, &w);
-                    v1--;
-                    v2--;
-                    grafo[v1][v2][0]=1;
-                    grafo[v1][v2][1]=w;
+                arestas = (Aresta *)malloc(m * sizeof(Aresta));
+                d = (int *)malloc(n * sizeof(int));
+                p = (int *)malloc(n * sizeof(int));
+
+                for (i = 0; i < m; i++) {
+                    fscanf(file, "%d %d %d", &arestas[i].v1, &arestas[i].v2, &arestas[i].w);
+                    arestas[i].v1--;
+                    arestas[i].v2--;
                 }
+                fclose(file);
                 break;
-		}
-	}
+        }
+    }
 
     printf("\nDigite o vértice inicial: ");
     scanf("%d", &s);
@@ -123,10 +108,17 @@ int main(int argc, char *argv[])
     printf("\nDigite o vértice final: ");
     scanf("%d", &v);
 
-    bellmanFord(s-1);
-    printf("\nO menor caminho de (%d) para (%d) é: \n\n",s,v);
+    if (bellmanFord(s - 1) == TRUE) {
+        printf("\nO menor caminho de (%d) para (%d) é:\n\n", s, v);
+        caminho(s - 1, v - 1);
+        printf("\n\nA distância mínima entre os vértices é %d\n", d[v - 1]);
+    } else {
+        printf("\nNão existe caminho de (%d) para (%d)\n", s, v);
+    }
+
+    free(arestas);
+    free(d);
+    free(p);
     
-    caminho(s-1,v-1);
-    printf("\n\nA distância mínima entre as duas arestas é %d\n",d[v-1]);
     return 0;
 }
